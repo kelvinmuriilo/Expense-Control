706c86308c58fec221863bee1df3f8cc97a86ed9
kelvinmuriilo <kelvinmuriloas@gmail.com>
2019-11-14 19:31:31 -0300

    iniciando configuração do frontend angular

---
+++ frontend/angular/frontend-expense/src/app/despesa/paginas/consulta-despesa/consulta-despesa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-despesa',
+  templateUrl: './consulta-despesa.component.html',
+  styleUrls: ['./consulta-despesa.component.scss']
+})
+export class ConsultaDespesaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
