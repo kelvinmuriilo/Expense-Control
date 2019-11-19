a6b2ee4a12e475a55045f8662a0bb506ce740adc
kelvinmuriilo <kelvinmuriloas@gmail.com>
2019-11-18 23:13:30 -0300

    corrigindo value input-selecao

--- frontend/angular/frontend-expense/src/app/compartilhado/componentes/input-selecao/input-selecao.component.ts
+++ frontend/angular/frontend-expense/src/app/compartilhado/componentes/input-selecao/input-selecao.component.ts
@@ -20,6 +20,26 @@ import {
 
 import { Tipo } from 'src/app/app.modelo';
 
+
+/**
+ * Componente utilizado para exibir uma selecao de opções.
+ * 
+ * Exemplo de como usar:
+ * <app-selecao-item
+      [formControl]="form.controls['selecao']"
+      [listaOpcao]="listaTeste"
+      (alterarValor)="teste($event)"
+      placeholder="Selecione o papel">
+    </app-selecao-item>
+  *
+  * @Input listaOpcao a lista de opções que sera exibida.
+  *   Deve ser uma lista do tipo {@link Array<Opcao>}
+  * @Input formControl a referencia de um control do FormGroup que esta sendo tratado.
+  * @Input classeIcone classe do icone do fontwasome que sera exibida.
+  * @Input placeholder texto do placeholder.
+  * @Output alterarValor evento que emit o valor do campo toda vez que o mesmo é alterado.
+  */
+
 @Component({
   selector: 'app-input-selecao',
   templateUrl: './input-selecao.component.html',
@@ -31,6 +51,7 @@ import { Tipo } from 'src/app/app.modelo';
   }]
 })
 export class InputSelecaoComponent implements OnInit, ControlValueAccessor {
+
 
   private readonly OPCAO_PLACEHOLDER: string = "placeholder";
 
